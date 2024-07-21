06b635610b68390ba18e57961465f9e3251c2336
Georg <georgestoneix@gmail.com>
2024-07-22 00:30:59 +0600

    Создал представления задач. Добавил get_search_value, get_context_data, get_form, get_success_url

---
+++ issuetracker/webapp/views/views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse
+from django.views.generic import CreateView, UpdateView, DeleteView, DetailView
+from webapp.forms import IssueForm
+from webapp.models import Issue, Project
+
+class IssueDetailView(DetailView):
+    template_name = "issue/issue_detail.html"
+    model = Issue
+    context_object_name = "issue"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        issue = self.get_object()
+        context['project'] = get_object_or_404(Project, id=issue.project.id)
+        return context
+
+class IssueCreateView(CreateView):
+    model = Issue
+    template_name = "issue/issue_create.html"
+    form_class = IssueForm
+
+    def form_valid(self, form):
+        form.instance.project = Project.objects.get(pk=self.kwargs['pk'])
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('project_detail', kwargs={'pk': self.kwargs['pk']})
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['project'] = Project.objects.get(pk=self.kwargs['pk'])
+        return context
+
+class IssueUpdateView(UpdateView):
+    template_name = "issue/issue_update.html"
+    form_class = IssueForm
+    model = Issue
+
+    def get_success_url(self):
+        return reverse("issue_detail", kwargs={"pk": self.object.pk})
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        issue = self.get_object()
+        context['project'] = get_object_or_404(Project, id=issue.project.id)
+        return context
+
+class IssueDeleteView(DeleteView):
+    template_name = "issue/issue_delete.html"
+    model = Issue
+
+    def get_success_url(self):
+        return reverse("project_detail", kwargs={"pk": self.object.project.pk})
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        issue = self.get_object()
+        context['project'] = get_object_or_404(Project, id=issue.project.id)
+        return context
