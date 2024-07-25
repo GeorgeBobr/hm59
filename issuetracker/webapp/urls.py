06c4d3885bc4d3b56495bc8753df86e09888faca
Georg <georgestoneix@gmail.com>
2024-07-25 16:33:30 +0600

    Обновил пути

--- issuetracker/webapp/urls.py
+++ issuetracker/webapp/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from .views.views import IssueDetailView, IssueCreateView, IssueUpdateView, IssueDeleteView
 from .views.project_views import (ProjectListView, ProjectDetailView, ProjectCreateView, ProjectUpdateView,
                                   ProjectDeleteView)
+from django.views.generic import RedirectView
 
 app_name = 'webapp'
 
 urlpatterns = [
-    path('', ProjectListView.as_view(), name='project_list'),
+    path('', RedirectView.as_view(pattern_name='webapp:project_list')),
+    path('peojects/', ProjectListView.as_view(), name='project_list'),
     path('projects/<int:pk>/', ProjectDetailView.as_view(), name='project_detail'),
     path('projects/create/', ProjectCreateView.as_view(), name='project_create'),
     path('projects/<int:pk>/update/', ProjectUpdateView.as_view(), name='project_update'),
