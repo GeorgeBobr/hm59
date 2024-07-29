e0b80fce6b0e1a6bdb6581699d4f939e826c5278
Georg <georgestoneix@gmail.com>
2024-07-29 16:44:58 +0600

    Добавил роли и ограничение для пользователей

--- issuetracker/webapp/views/project_views.py
+++ issuetracker/webapp/views/project_views.py
@@ -1,7 +1,7 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.db.models import Q
 from django.shortcuts import redirect
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.utils.http import urlencode
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from webapp.forms import ProjectForm, SearchForm
@@ -53,23 +53,37 @@ class ProjectDetailView(DetailView):
         context["issues"] = Issue.objects.filter(project=self.object)
         return context
 
-
 class ProjectCreateView(LoginRequiredMixin, CreateView):
     template_name = "project/project_create.html"
     form_class = ProjectForm
     success_url = reverse_lazy('webapp:project_list')
 
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
-class ProjectUpdateView(LoginRequiredMixin, UpdateView):
+    def get_success_url(self):
+        return reverse('webapp:project_detail', kwargs={'pk': self.object.pk})
+
+
+class ProjectUpdateView(PermissionRequiredMixin, UpdateView):
     template_name = "project/project_update.html"
     form_class = ProjectForm
     model = Project
+    permission_required = "webapp.change_project"
+
+    def has_permission(self):
+        return super().has_permission() or self.request.user == self.get_object().author
 
     def get_success_url(self):
         return reverse_lazy('webapp:project_detail', kwargs={'pk': self.object.pk})
 
 
-class ProjectDeleteView(LoginRequiredMixin, DeleteView):
+class ProjectDeleteView(PermissionRequiredMixin, DeleteView):
     template_name = "project/project_delete.html"
     model = Project
     success_url = reverse_lazy('webapp:project_list')
+    permission_required = "webapp.delete_project"
+
+    def has_permission(self):
+        return super().has_permission() or self.request.user == self.get_object().author
