1a22d96ca1fe818be86e1d20a14fcf79ea306a44
Georg <georgestoneix@gmail.com>
2024-07-29 16:45:25 +0600

    Создал форму создания юзера, добавил ему валидаторы

--- issuetracker/webapp/forms.py
+++ issuetracker/webapp/forms.py
@@ -1,10 +1,11 @@
-from django import forms
 from django.forms import widgets
 from django.core.validators import BaseValidator
 from django.core.exceptions import ValidationError
-
 from webapp.models import Type, Status, Project, Issue
+from django import forms
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 class IssueForm(forms.ModelForm):
     class Meta:
         model = Issue
@@ -18,6 +19,14 @@ class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
         fields = ['start_data', 'end_data', 'title', 'description']
+
+
+class ProjectUserForm(forms.ModelForm):
+    users = forms.ModelMultipleChoiceField(queryset=User.objects.all(), widget=forms.CheckboxSelectMultiple)
+
+    class Meta:
+        model = Project
+        fields = ['users']
 
 class StatusForm(forms.Form):
     title = forms.CharField(max_length=50, required=True, label="Название")
